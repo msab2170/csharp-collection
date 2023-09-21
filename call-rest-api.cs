e36aafad6e82663cc46703f8f13667bf5f904710
moonster <115135514+msab2170@users.noreply.github.com>
2023-09-21 21:26:31 +0900

    Update call-rest-api.cs

--- call-rest-api.cs
+++ call-rest-api.cs
@@ -1,4 +1,4 @@
-public async Task<int> HTTPGet(타입1 변수1,...){
+public async Task<int> HTTPGet(타입1 변수1,...){  // 가장 단순한 Get 방식
   int result = 0;
 
   string domain = "https://abcd.dcba.com";
@@ -11,13 +11,13 @@ public async Task<int> HTTPGet(타입1 변수1,...){
   
   try
   {
-    HttpClient client = new();
-    HttpResponseMessage response = await client.GetAsync(reqeustUrl);
+    HttpClient client = new();    // Header 관련도 곧 추가 예정
+    HttpResponseMessage response = await client.GetAsync(reqeustUrl);  //requestUrl 경로로 Get 방식으로 request 후 response를 받음
   
-    if (response.StatusCode == HttpStatusCode.OK)
+    if (response.StatusCode == HttpStatusCode.OK)  // 응답상태코드가 200이면
     {
-      var responseContent = await response.Content.ReadAsStringAsync();
-      var 받은변수 = JsonConvert.DeserializeObject<받을타입model이 있다면 여기에>(responseContent);
+      var responseContent = await response.Content.ReadAsStringAsync();    // 문자열로 받아온 responseBody
+      var 받은변수 = JsonConvert.DeserializeObject<받을타입model이 있다면 여기에>(responseContent);  // 받아온 문자열이 제이슨 형태라면 Deserialize하여 타입변수에 넣을 수 있음
       Log.Information($"[{response.StatusCode}]{responseContent}");
       result = 1;
     }
