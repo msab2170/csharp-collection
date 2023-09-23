43d126e2402fc6ee46984f859f9d0c92f27f72b9
moonster <115135514+msab2170@users.noreply.github.com>
2023-09-23 18:16:05 +0900

    Update call-rest-api.cs

--- call-rest-api.cs
+++ call-rest-api.cs
@@ -13,13 +13,15 @@ public async Task<int> HTTPGet(타입1 변수1,...){  // 가장 단순한 Get �
   {
     HttpClient client = new();    // Header 관련도 곧 추가 예정
     HttpResponseMessage response = await client.GetAsync(reqeustUrl);  //requestUrl 경로로 Get 방식으로 request 후 response를 받음
-  
+
+    var responseContent = await response.Content.ReadAsStringAsync();    // 문자열로 받아온 responseBody     
+    Log.Information($"[{response.StatusCode}]{responseContent}");
+    
     if (response.StatusCode == HttpStatusCode.OK)  // 응답상태코드가 200이면
-    {
-      var responseContent = await response.Content.ReadAsStringAsync();    // 문자열로 받아온 responseBody
-      var 받은변수 = JsonConvert.DeserializeObject<받을타입model이 있다면 여기에>(responseContent);  // 받아온 문자열이 제이슨 형태라면 Deserialize하여 타입변수에 넣을 수 있음
-      Log.Information($"[{response.StatusCode}]{responseContent}");
+    {      
       result = 1;
+       var 받은변수 = JsonConvert.DeserializeObject<받을타입model이 있다면 여기에>(responseContent);  // 받아온 문자열이 제이슨 형태라면 Deserialize하여 타입변수에 넣을 수 있음
+      // 요청이 성공해야 원하는 형태의 데이터가 오기때문에 분기로 거른다음에 받아야함
     }
     else
     {
