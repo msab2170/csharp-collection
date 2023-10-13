9f302f4ae90c176a66981397bc0b8475f9b43430
moonster <115135514+msab2170@users.noreply.github.com>
2023-10-13 11:20:10 +0900

    Update ConfigHandler.cs

--- ConfigHandler.cs
+++ ConfigHandler.cs
@@ -1,70 +1,74 @@
+/* 
+    결국 이전 버전에서는 ini파일을 초기에만 읽어와 Dictionary에 담는 방법을 썼는데
+    서비스가 유지중인 곳에서 쓸때는 config파일의 내용을 수정하면 반영이 안되기 때문에
+    그때 그때 읽어오도록 수정했다.
+
+    참고로 string value = line[(line.IndexOf(":") + 1)..].Trim(); // 이 문법은 구버전에서는 작동하지 않을 수 있다. 그럴때에는 Substring()를 사용하면 된다.
+*/
+
 class ConfigHandler
 {
     public static readonly string configFile = "설정파일";             // 설정파일경로\설정파일명
-    public static Dictionary<string, string> configData = new(); // 설정 정보 저장을 위한 Dictionary
 
-    // 설정파일 a:b꼴로 저장한 값을 읽어와 configData에 저장
-    public static void ReadConfig()
+    // 설정파일에 a:b꼴로 저장한 값을 return
+    public static string ReadConfig(string configInfo)
     {
         if (File.Exists(configFilePath))
         {
             try
             {
                 string[] lines = File.ReadAllLines(configFilePath);
-
+                
                 foreach (string line in lines)
                 {
-                    // 빈 줄이면 패스, #는 config.txt 주석 처리
                     if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
                         continue;
-
-                    if (line.Contains(":"))
+    
+                    if (line.Contains(':'))
                     {
-                        string key = line.Substring(0, line.IndexOf(":")).Trim();
-                        string value = line.Substring(line.IndexOf(":") + 1).Trim();
-                        configData[key] = value;
+                        string key = line[..line.IndexOf(":")].Trim() ?? throw new Exception($"ConfigHandler.ReadConfig - ReadConfig({configInfo}) failed."); 
+    
+                        if (key.Equals(configInfo))
+                        {
+                            string value = line[(line.IndexOf(":") + 1)..].Trim();    
+                            Log.Information($"ConfigHandler.ReadConfig(\"{configInfo}\")={value}");
+                            return value;
+                        }
                     }
                 }
             }
             catch (Exception ex)
             {
-                throw new Exception($"ConfigHandler.readConfig - An error occurred while reading file[{configFile}]: {ex.Message}");
+                Log.Error($"ConfigHandler.readConfig - An error occurred while reading the config file[{configFilePath}]: {ex.Message}");
             }
         }
         else
         {
-            throw new Exception($"ConfigHandler.readConfig - [{configFile}] file not found.");
+            throw new Exception($"ConfigHandler.readConfig - [{configFilePath}] file not found.");
         }
+        throw new Exception($"ConfigHandler.readConfig - maybe [{configInfo}] not exists in [{configFilePath}].");
     }
-}
+
 
 // 설정 파일에 a:asdf,wqer,fr 꼴로 여러개를 입력받아 배열로 만드는 메소드
-public static string[] transformConfigData(string configString)
+public static string[] TransformConfigData(string configKey)
 {
     string[] ConfigDataArr;
-    if (!string.IsNullOrEmpty(HandleConfiguration.configData[configString])
-        && HandleConfiguration.configData[configString].Contains(","))
+    string configValue = ReadConfig(configKey);
+          
+    if (!string.IsNullOrEmpty(configValue) && configValue.Contains(','))
     {
-        ConfigDataArr = HandleConfiguration.configData[configString].Split(',');
+        ConfigDataArr = configValue.Split(',');
     }
-    else if (!string.IsNullOrEmpty(HandleConfiguration.configData[configString]))
+    else if (!string.IsNullOrEmpty(configValue))
     {
         ConfigDataArr = new string[1];
-        ConfigDataArr[0] = HandleConfiguration.configData[configString];
+        ConfigDataArr[0] = configValue;
     }
     else
     {
-        throw new Exception($"{configString} is null or Empty!");
+        throw new Exception($"the value that key is [{configKey}] in [{configFilePath}] is necessary.");
     }
     return ConfigDataArr;
 }
-// Split() 을 편하게 쓰고자 배열로 만들었고, 
-// 필자가 작업시에는 해당정보가 없으면 프로그램이 돌아가면 안되는 경우가 보통이라서 null or empty일때 exception을 던지는 경우가 많다. 
-// 각자 사용하기 편한 자료형으로 만들면 될듯!
 
-//-------------------------------------------------------
-// Program.cs 
-ConfigHandler.ReadConfig(); 
-string a = ConfigHandler.configData["아무변수"];
-
-// 서버를 재시작하지 않고 설정파일을 변경하고 싶은경우 ReadConfig()에 변수를 담아 메소드가 실행될때마다 읽어오게 하면 된다.
