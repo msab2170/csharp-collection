b9cc8f7cc118be7166a6f76abb99778badcae111
moonster <115135514+msab2170@users.noreply.github.com>
2023-11-03 15:05:43 +0900

    Update OutlookAddinProject.cs

--- OutlookAddinProject.cs
+++ OutlookAddinProject.cs
@@ -9,13 +9,17 @@ using Office = Microsoft.Office.Core;
 
 /*
   여기에 적혀있는 것들은 대부분 outlookAddIn 프로젝트를 만들면 들어있는 내용이다.
-  이메일을 보냈을때, 특정 로직을 수행하는 방법을 외워두기는 자주쓰는 내용은 아니고, 찾기는 부정확한 정보도 많은 것 같아 정리했다.
+  이메일을 보냈을때(보내기 직전과 보낸 직후), 특정 로직을 수행하는 방법을 외워두기는 자주쓰는 내용은 아니고, 찾기는 부정확한 정보도 많은 것 같아 정리했다.
+  특히, 보낸이와 받는이의 displayName이 아닌 이메일 주소 자체를 가져오려면 ms 공식홈페이지의 내용과 다른사람의 경험 등을 짜집기할 필요가 있었다.
 */
 namespace 네임스페이스명
 {
     public partial class ThisAddIn
     {
+        private MAPIFolder _sentItemsFolder;
+        private Items _items;
 
+        
         /// <summary>
         /// Outlook이 실행될때 ThisAddIn_Startup가 작동한다.
         /// </summary>
@@ -27,8 +31,31 @@ namespace 네임스페이스명
 
             // 이 예제에서는 "이메일 보낼때"를 감지하는 이벤트에 이메일 보낼때 실행시킬 메소드를 부착한다.
             this.Application.ItemSend += new Outlook.ApplicationEvents_11_ItemSendEventHandler(Application_ItemSend);
+            this.Application.Startup += new ApplicationEvents_11_StartupEventHandler(Application_Startup);            // outlook이 동작할때 이벤트 부착
         }
 
+
+        
+        private void Application_Startup()
+        {
+            NameSpace outlookNameSpace = this.Application.GetNamespace("MAPI");               
+            _sentItemsFolder = outlookNameSpace.GetDefaultFolder(OlDefaultFolders.olFolderSentMail);  // 보낸 메일함을 변수에 담
+            _items = _sentItemsFolder.Items; // 메서드에서 할당
+            _items.ItemAdd += new ItemsEvents_ItemAddEventHandler(SentItemsFolder_ItemAdd); // 아웃룩이 올라올때 보낸메일함에 메일추가될때 이벤트 부착
+        }
+
+
+        private void SentItemsFolder_ItemAdd(object Item)    // 보낸메일함에 이메일이 추가됐을때 발동되는 함수로 만일 이메일 발송시 보낸메일함에 저장이 되지않는다면 보낸 직후에 이벤트를 이것으로는 실행되지 못한다.
+        {
+            // Check if the item is a mail item
+            if (Item is MailItem)
+            {
+                MailItem sentMail = Item as MailItem;
+                // 보낸 메일함에 추가된 메일의 정보를 가져올 수 있다.
+            }
+        }
+
+        
         /// <summary>
         /// 이메일 보낼때 실행할 메소드
         /// </summary>
@@ -48,6 +75,22 @@ namespace 네임스페이스명
                     else                                        sender = email.SendUsingAccount.SmtpAddress;    
                     //
 
+                    // 참고로 수신자들도 EX로 시작하는 값이 올 수 있어서 특별한 방법으로 가져올 필요가 있다.
+                    Recipients recipients = email.Recipients;
+                    var recipientsSet = new HashSet<string>();    // 이 예제에서는 확인된 수신자를 Set에 담았는데 독자가 판단해서 필요한 컬렉션 등에 넣으면 된다.
+                    foreach (Recipient recipient in recipients)
+                    {
+                        if (recipient.Address.Contains('@'))
+                        {
+                            recipientsSet.Add(recipient.Address);
+                        }
+                        else 
+                        {
+                            PropertyAccessor propertyAccessor = recipient.PropertyAccessor;
+                            string smtpAddress = propertyAccessor.GetProperty(PR_SMTP_ADDRESS).ToString().Trim();
+                            recipientsSet.Add(smtpAddress);
+                        }
+                    }
                 }
             }
             catch (System.Exception ex)
